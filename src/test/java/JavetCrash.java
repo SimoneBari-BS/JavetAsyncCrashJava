1a5691639cde57da820e102fe1c48260c567cbd0
simonebari <simb@bendingspoons.com>
2023-10-02 15:35:08 +0200

    Resolve leaks

--- src/test/java/JavetCrash.java
+++ src/test/java/JavetCrash.java
@@ -9,7 +9,11 @@ import com.caoccao.javet.values.reference.V8ValuePromise;
 import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.Test;
 
+import java.util.concurrent.ConcurrentLinkedQueue;
+
 public class JavetCrash {
+
+    private static ConcurrentLinkedQueue<V8ValuePromise> queue = new ConcurrentLinkedQueue<>();
 
     @Test
     public void testJavetCrash() throws JavetException, NoSuchMethodException {
@@ -24,33 +28,45 @@ public class JavetCrash {
                     "var complexClass = {'hello': 'hello'}; return context.invoke(complexClass); }"
             ).executeVoid();
 
-            for (int i = 0; i < 100000; i++) {
+            final int REPEAT_COUNT = 10000;
+            for (int i = 0; i < REPEAT_COUNT; i++) {
                 System.out.println("Repeat iteration #" + i);
-                try (V8Value result = runtime.getGlobalObject().invoke("main", context);) {
+                try (V8Value ignored = runtime.getGlobalObject().invoke("main", context)) {
                     Thread.sleep(0);
                 } catch (InterruptedException e) {
                     throw new RuntimeException(e);
                 }
             }
+
+            try {
+                while (queue.size() < REPEAT_COUNT) {
+                    Thread.sleep(10);
+                }
+                while (!queue.isEmpty()) {
+                    V8ValuePromise promise = queue.poll();
+                    while (!promise.isFulfilled()) {
+                        Thread.sleep(10);
+                    }
+                    promise.close();
+                }
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+
+            System.out.println("AAA");
         }
+
+
     }
 
     @NotNull
     private static JavetCallbackContext getJavetCallbackContext(V8Runtime runtime) throws NoSuchMethodException {
-        AsyncMethodWrapper callbackObject = new AsyncMethodWrapper(runtime, parameter -> {
-            Thread thread = new Thread(() -> {
-                System.out.println("Executing async function");
-                try {
-                    String hello = parameter.getString("hello");
-                    assert hello.equals("hello");
-                } catch (JavetException e) {
-                    throw new RuntimeException(e);
-                }
-            });
-            thread.start();
+        AsyncMethodWrapper callbackObject = new AsyncMethodWrapper(runtime, queue, parameter -> {
+            System.out.println("Executing async function");
             try {
-                thread.join();
-            } catch (InterruptedException e) {
+                String hello = parameter.getString("hello");
+                assert hello.equals("hello");
+            } catch (JavetException e) {
                 throw new RuntimeException(e);
             }
             try {
@@ -67,10 +83,16 @@ public class JavetCrash {
 class AsyncMethodWrapper {
     private final V8Runtime runtime;
     private final java.util.function.Function<V8ValueObject, V8Value> block;
+    private final ConcurrentLinkedQueue<V8ValuePromise> promisesQueue;
 
-    public AsyncMethodWrapper(V8Runtime runtime, java.util.function.Function<V8ValueObject, V8Value> block) {
+    public AsyncMethodWrapper(
+            V8Runtime runtime,
+            ConcurrentLinkedQueue<V8ValuePromise> promiseQueue,
+            java.util.function.Function<V8ValueObject, V8Value> block
+    ) {
         this.runtime = runtime;
         this.block = block;
+        this.promisesQueue = promiseQueue;
     }
 
     public V8Value invoke(V8ValueObject parameter) {
@@ -86,7 +108,8 @@ class AsyncMethodWrapper {
                 } catch (JavetException | InterruptedException e) {
                     throw new RuntimeException(e);
                 } finally {
-                    JavetResourceUtils.safeClose(promise, clonedParam);
+                    JavetResourceUtils.safeClose(clonedParam);
+                    promisesQueue.add(promise);
                 }
             });
             thread.start();
